4201733d90000c6a5017b304b4427259f49bd9cf
Markus <markus.maelzer@gmail.com>
2020-03-08 19:38:56 +0100

    fix type error

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,12 +1,14 @@
 import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+const Test = () => <div>asd</div>;
+
 export const MainRouter = () => {
   return (
     <Suspense fallback={null}>
       <div className="main-content">
         <Switch>
-          <Route exact path="/" component={<div></div>} />
+          <Route exact path="/" component={Test} />
         </Switch>
       </div>
     </Suspense>
