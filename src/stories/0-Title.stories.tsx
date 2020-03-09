93f50fd17187f3959ff3bb0f68ea15e808ad2187
XImOnTRiX <domenik.wenge@gmail.com>
2020-03-09 17:47:58 +0100

    added storybook typescript support

---
+++ src/stories/0-Title.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Title } from "ui/index";
+
+export default {
+  title: "Title",
+};
+
+export const Typography = () => (
+  <div>
+    <Title type="h1" style={{color: "blue"}}>test</Title>
+  </div>
+);
+
