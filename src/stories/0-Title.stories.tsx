d8c7be055bdf1fdc0b75fc424acdc6756cec0c4e
XImOnTRiX <domenik.wenge@gmail.com>
2020-03-19 16:12:09 +0100

    added Title components to storybook

--- src/stories/0-Title.stories.tsx
+++ src/stories/0-Title.stories.tsx
@@ -12,6 +12,21 @@ export const TitleH1BisH6 = () => (
     <Title type="h1" style={{ color: "blue" }}>
       TEST (づ｡◕__◕｡)づ
     </Title>
+    <Title type="h2" style={{ color: "blue" }}>
+      TEST (づ｡◕__◕｡)づ
+    </Title>
+    <Title type="h3" style={{ color: "blue" }}>
+      TEST (づ｡◕__◕｡)づ
+    </Title>
+    <Title type="h4" style={{ color: "blue" }}>
+      TEST (づ｡◕__◕｡)づ
+    </Title>
+    <Title type="h5" style={{ color: "blue" }}>
+      TEST (づ｡◕__◕｡)づ
+    </Title>
+    <Title type="h6" style={{ color: "blue" }}>
+      TEST (づ｡◕__◕｡)づ
+    </Title>
   </div>
 );
 
