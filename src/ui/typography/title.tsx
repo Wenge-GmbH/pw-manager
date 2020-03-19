cd933737875735b87146f2e6543b9c4b17f5107f
Markus <markus.maelzer@gmail.com>
2020-03-19 14:52:32 +0100

    add type

--- src/ui/typography/title.tsx
+++ src/ui/typography/title.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import styled from "styled-components";
-import { theme } from "../theme";
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../theme';
 
-const querys = ["sm", "md", "lg", "xl"];
+const querys = ['sm', 'md', 'lg', 'xl'];
 
-const fontSize = (key) => {
+const fontSize = (key: string) => {
   return querys.map(data => {
     return `@media (min-width: ${theme.mediaQuery[data]}px) {
       font-size: ${theme.fontSize[key][data]}
-    }`
-  })
+    }`;
+  });
 };
 
 type TitleComponent = {
@@ -19,43 +19,43 @@ type TitleComponent = {
 
 const H1 = styled.h1`
   font-size: 8pt;
-  color: ${props => props.theme.color || "black"};
-  ${fontSize("h1")};
+  color: ${props => props.theme.color || 'black'};
+  ${fontSize('h1')};
 `;
 const H2 = styled.h2`
-  font-size: "12pt";
-  color: ${props => props.theme.color || "black"};
+  font-size: '12pt';
+  color: ${props => props.theme.color || 'black'};
 `;
 const H3 = styled.h4`
-  font-size: "12pt";
-  color: ${props => props.theme.color || "black"};
+  font-size: '12pt';
+  color: ${props => props.theme.color || 'black'};
 `;
 const H4 = styled.h4`
-  font-size: "12pt";
-  color: ${props => props.theme.color || "black"};
+  font-size: '12pt';
+  color: ${props => props.theme.color || 'black'};
 `;
 const H5 = styled.h5`
-  font-size: "12pt";
-  color: ${props => props.theme.color || "black"};
+  font-size: '12pt';
+  color: ${props => props.theme.color || 'black'};
 `;
 const H6 = styled.h6`
-  font-size: "12pt";
-  color: ${props => props.theme.color || "black"};
+  font-size: '12pt';
+  color: ${props => props.theme.color || 'black'};
 `;
 
 const Title: React.FC<TitleComponent> = ({ type, children }) => {
   switch (type) {
-    case "h1":
+    case 'h1':
       return <H1>{children}</H1>;
-    case "h2":
+    case 'h2':
       return <H2>{children}</H2>;
-    case "h3":
+    case 'h3':
       return <H3>{children}</H3>;
-    case "h4":
+    case 'h4':
       return <H4>{children}</H4>;
-    case "h5":
+    case 'h5':
       return <H5>{children}</H5>;
-    case "h6":
+    case 'h6':
       return <H6>{children}</H6>;
     default:
       return null;
@@ -63,7 +63,7 @@ const Title: React.FC<TitleComponent> = ({ type, children }) => {
 };
 
 Title.defaultProps = {
-  type: "h1"
+  type: 'h1'
 };
 
 export default Title;
