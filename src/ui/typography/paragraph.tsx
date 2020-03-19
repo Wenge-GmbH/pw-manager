b446ba200592a2ee26af0cb5a9899c3b787ef2ec
XImOnTRiX <domenik.wenge@gmail.com>
2020-03-19 16:06:00 +0100

    2 more files

---
+++ src/ui/typography/paragraph.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../theme";
+
+const fontSize = (key: string) => {
+  return Object.keys(theme.mediaQuery).map(data => {
+    return `@media (min-width: ${theme.mediaQuery[data]}px) {
+      font-size: ${theme.fontSize[key][data]}
+    }`;
+  });
+};
+
+type ParagraphComponent = {
+  style?: object;
+};
+
+const Para = styled.p`
+  font-size: 8pt;
+  color: ${props => props.theme.color || "black"};
+  ${fontSize("p")};
+`;
+
+const Paragraph: React.FC<ParagraphComponent> = ({ children }) => (
+    <Para>{children}</Para>
+)
+
+
+export default Paragraph;
---
+++ src/ui/typography/text.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../theme";
+
+const fontSize = (key: string) => {
+  return Object.keys(theme.mediaQuery).map(data => {
+    return `@media (min-width: ${theme.mediaQuery[data]}px) {
+      font-size: ${theme.fontSize[key][data]}
+    }`;
+  });
+};
+
+type TxtComponent = {
+  style?: object;
+};
+
+const Txt = styled.p`
+  font-size: 8pt;
+  color: ${props => props.theme.color || "black"};
+  ${fontSize("p")};
+`;
+
+const Text: React.FC<TxtComponent> = ({ children }) => (
+  <Txt>{children}</Txt>
+);
+
+export default Text;
