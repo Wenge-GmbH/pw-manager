a102c11dea3b7d263c52c4351d1e3f70d64b6335
XImOnTRiX <domenik.wenge@gmail.com>
2020-03-19 16:42:02 +0100

    small refactor to add custom styling

--- src/ui/typography/title.tsx
+++ src/ui/typography/title.tsx
@@ -52,20 +52,20 @@ const H6 = styled.h6`
   ${fontSize("h6")};
 `;
 
-const Title: React.FC<TitleComponent> = ({ type, children }) => {
-  switch (type) {
+const Title: React.FC<TitleComponent> = (props) => {
+  switch (props.type) {
     case 'h1':
-      return <H1>{children}</H1>;
+      return <H1 {...props} >{props.children}</H1>;
     case 'h2':
-      return <H2>{children}</H2>;
+      return <H2 {...props} >{props.children}</H2>;
     case 'h3':
-      return <H3>{children}</H3>;
+      return <H3 {...props} >{props.children}</H3>;
     case 'h4':
-      return <H4>{children}</H4>;
+      return <H4 {...props} >{props.children}</H4>;
     case 'h5':
-      return <H5>{children}</H5>;
+      return <H5 {...props} >{props.children}</H5>;
     case 'h6':
-      return <H6>{children}</H6>;
+      return <H6 {...props} >{props.children}</H6>;
     default:
       return null;
   }
