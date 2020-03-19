48d5dbfb1c03994038ed526ec0cfea243f942b03
XImOnTRiX <domenik.wenge@gmail.com>
2020-03-19 16:23:10 +0100

    changed fontsizes

--- src/ui/theme/index.ts
+++ src/ui/theme/index.ts
@@ -34,12 +34,12 @@ export const theme: theme = {
     tableFlip: '(ノಠ益ಠ)ノ彡┻━┻'
   },
   fontSize: {
-    h1: { sm: '10pt', md: '12pt', lg: '14pt', xl: '16pt' },
-    h2: { sm: '10pt', md: '12pt', lg: '14pt', xl: '16pt' },
-    h3: { sm: '10pt', md: '12pt', lg: '14pt', xl: '16pt' },
-    h4: { sm: '10pt', md: '12pt', lg: '14pt', xl: '16pt' },
-    h5: { sm: '10pt', md: '12pt', lg: '14pt', xl: '16pt' },
-    h6: { sm: '10pt', md: '12pt', lg: '14pt', xl: '16pt' }
+    h1: { sm: '16pt', md: '20pt', lg: '22pt', xl: '24pt' },
+    h2: { sm: '15pt', md: '19pt', lg: '21pt', xl: '23pt' },
+    h3: { sm: '14pt', md: '18pt', lg: '20pt', xl: '22pt' },
+    h4: { sm: '13pt', md: '17pt', lg: '19pt', xl: '21pt' },
+    h5: { sm: '12pt', md: '16pt', lg: '18pt', xl: '20pt' },
+    h6: { sm: '11pt', md: '15pt', lg: '17pt', xl: '19pt' }
   },
   mediaQuery: {
     sm: 600,
