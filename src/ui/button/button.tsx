8dd8e9c8bfd01eb5106fb8469fa79fa37a7ab158
XImOnTRiX <domenik.wenge@gmail.com>
2020-04-13 19:15:14 +0200

    added box shadow

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -25,15 +25,23 @@ type ButtonComponent = {
 };
 
 const Btn = styled.button`
+  cursor: pointer;
+  transition: 200ms;
   font-family: ${theme.font.secondary};
-  padding: 11px 35px 11px 35px;
-  ${padding()};
   font-weight: 700;
   background: ${theme.color.purple};
   border: ${theme.button.border};
   border-radius: ${theme.button.borderRadius};
   color: ${(props) => props.theme.color || "#EDF1F7"};
+  box-shadow: 0px 1px 2px rgba(0,0,0,0.1); 
+  padding: 11px 35px 11px 35px;
+  ${padding()};
   ${fontSize("button")};
+
+  &:hover {
+    transition: 200ms;
+    background: ${theme.color.purpleAccent};
+  }
 `;
 
 const Button: React.FC<ButtonComponent> = ({ children }) => (
